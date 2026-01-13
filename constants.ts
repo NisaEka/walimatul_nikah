1fd6db32b9fe679efec3d06cabb93fd245e8320d
nisae <nisa.eka.juliana@gmail.com>
2026-01-13 16:22:18 +0700

    ganti lagu

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 export const WEDDING_DATE = new Date('2026-02-01T08:00:00'); // January 1, 2026
 export const GOOGLE_MAPS_URL = "https://www.google.com/maps/search/?api=1&query=Masjid+Nahdjussalam+Jl.+Panyawungan+Cileunyi+Wetan";
 // Using a royalty-free placeholder music. Replace with your own specific URL.
-export const MUSIC_URL = "/walimatul_nikah/music/akad.mp3";
+export const MUSIC_URL = "/walimatul_nikah/music/dekat.mp3";
 
 export const BRIDE_NAME = "Nisa Eka Juliana";
 export const GROOM_NAME = "Fiqri Rizal Zulmi";
